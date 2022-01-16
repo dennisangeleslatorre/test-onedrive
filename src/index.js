9d2b2f0a1ed1e196375c62fc7b937b7c20782b30
Dennis Angeles <denisarmando203@gmail.com>
2022-01-15 20:43:36 -0500

    Login onedrive

--- src/index.js
+++ src/index.js
@@ -2,7 +2,13 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
-import reportWebVitals from './reportWebVitals';
+import { Providers } from '@microsoft/mgt-element';
+import { Msal2Provider } from '@microsoft/mgt-msal2-provider';
+
+//Inicializar microsoft Graph Toolkit con el proveedor MSAL
+Providers.globalProvider = new Msal2Provider({
+  clientId: '1c8ca553-f56e-4052-addd-d2d68493bd9f'
+});
 
 ReactDOM.render(
   <React.StrictMode>
@@ -10,8 +16,3 @@ ReactDOM.render(
   </React.StrictMode>,
   document.getElementById('root')
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
